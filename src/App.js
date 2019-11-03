411035aa96fbdf84153dc315670acdd1c2e6a0b7
Rajdeep Sharma <rajdeep.sharma@mechademy.com>
2019-11-03 21:05:27 +0530

    sfsdaf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import Button from './Button'
+import request from 'request'
 
 function App() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
-
+  request
+  .get('https://my-json-server.typicode.com/typicode/demo/posts')
+  .on('response', function(response) {
+    console.log(response.toJSON())
+  })
   return (
     <div>
       <p>
